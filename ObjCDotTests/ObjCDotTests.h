ebaadd94f1a6682983baa7d62026530a408084fb
Graham Lee <graham@agant.com>
2013-01-16 18:24:04 +0000

    Create a framework target, and some tests to ensure I understand how the code works.

---
+++ ObjCDotTests/ObjCDotTests.h
@@ -0,0 +1,13 @@
+//
+//  ObjCDotTests.h
+//  ObjCDotTests
+//
+//  Created by Graham Lee on 16/01/2013.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ObjCDotTests : SenTestCase
+
+@end
---
+++ ObjCDotTests/ObjCDotTests.m
@@ -0,0 +1,60 @@
+//
+//  ObjCDotTests.m
+//  ObjCDotTests
+//
+//  Created by Graham Lee on 16/01/2013.
+//
+//
+
+#import "ObjCDotTests.h"
+#import "OCVField.h"
+
+@interface ExampleClass : NSObject
+{
+    @public
+    int integerVariable;
+}
+@end
+@implementation ExampleClass
+@end
+
+@implementation ObjCDotTests
+{
+    NSObject *_obj;
+    ExampleClass *_example;
+    NSArray *_fieldsInNSObject;
+    NSArray *_fieldsInExampleClass;
+}
+
+- (void)setUp
+{
+    _obj = [NSObject new];
+    _example = [ExampleClass new];
+    _fieldsInNSObject = [_obj getFields];
+    _fieldsInExampleClass = [_example getFields];
+}
+
+- (void)tearDown
+{
+    _fieldsInExampleClass = nil;
+    _fieldsInNSObject = nil;
+    [_obj release];
+    _obj = nil;
+    [_example release];
+    _example = nil;
+}
+
+- (void)testOneFieldAddedForOneIvar
+{
+    STAssertEquals([_fieldsInExampleClass count] - [_fieldsInNSObject count], (NSUInteger)1, @"NSObject has %ld fields while ExampleClass has %ld", (long)[_fieldsInNSObject count], (long)[_fieldsInExampleClass count]);
+}
+
+- (void)testNSObjectHasIsaField
+{
+    OCVField *isaField = _fieldsInNSObject[0];
+    STAssertEqualObjects([isaField getName], @"isa", @"NSObject's ivar is the 'isa' variable");
+    STAssertFalse([isaField isPrimitive], @"isa is a class, not a primitive");
+    STAssertEqualObjects([isaField getValueForObject: _example], [ExampleClass class], @"isa has the expected value of the object's class");
+}
+
+@end
